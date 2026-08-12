50963f0ef54b13e78cb6a4822b31c5a67dc54605
Guillaume Holley <guillaumeh@olv-guillaumeh.decode.is>
2026-08-12 14:37:12 +0000

    Add options to mask low-quality corrected sequences in addition to the splitting options.

--- scripts/splitAtLowBQ.py
+++ scripts/splitAtLowBQ.py
@@ -11,21 +11,27 @@ import multiprocessing as mp
 from itertools import groupby
 from operator import itemgetter
 
+def printRecord(title, seq, qual):
+
+	print("@" + title) # Name
+	print(seq) # Sequence
+	print("+")
+	print(qual) # Qualities
+
+def printRecords(l_records):
+
+	for rec in l_records:
+
+		print("@" + rec[0]) # Name
+		print(rec[1]) # Sequence
+		print("+")
+		print(rec[2]) # Qualities
+
 def replaceIUPAC(seq=None):
 
-	dna_alpha=['A', 'C', 'G', 'T']
+	dna_alpha=('A', 'C', 'G', 'T')
 	
 	return ''.join([c if c in dna_alpha else random.choice(dna_alpha) for c in seq.upper()])
-
-#def getLowBQIntervals(qual, min_bq, max_len_lowbq):
-#
-#	qual_int = [ord(x)-33 for x in qual]
-#	qual_bool = [(x >= min_bq) for x in qual_int]
-#	
-#	runs_lowBQ = [[i for i, _ in group] for key, group in groupby(enumerate(qual_bool), key=itemgetter(1)) if not key]
-#	itv_runs_lowBQ = [(x[0], x[-1]) for x in runs_lowBQ if (len(x)>=max_len_lowbq)]
-#	
-#	return itv_runs_lowBQ
 
 def getLowBQIntervals(qual, min_bq, max_len_lowbq, max_ratio_low_bq):
 
@@ -52,70 +58,73 @@ def getLowBQIntervals(qual, min_bq, max_len_lowbq, max_ratio_low_bq):
 	runs_lowBQ = [[i for i, _ in group] for key, group in groupby(enumerate(qual_bool_windows), key=itemgetter(1)) if not key]
 	itv_runs_lowBQ = [(x[0], x[-1] + max_len_lowbq) for x in runs_lowBQ]
 
-	if (len(itv_runs_lowBQ) <= 1): return itv_runs_lowBQ
+	if len(itv_runs_lowBQ) <= 1: return itv_runs_lowBQ
 	else:
-
 		# Merge overlapping intervals
 		itv_runs_lowBQ.sort(key=lambda x: x[0])
 		merged_itv_runs_lowBQ = [itv_runs_lowBQ[0]]
 
-		for curr in itv_runs_lowBQ:
+		for curr_s, curr_e in itv_runs_lowBQ:
 
-			prev = merged_itv_runs_lowBQ[-1]
+			prev_s, prev_e = merged_itv_runs_lowBQ[-1]
 
-			if (curr[0] <= prev[1]): merged_itv_runs_lowBQ[-1] = (prev[0], max(prev[1], curr[1]))
-			else: merged_itv_runs_lowBQ.append(curr)
+			if curr_s <= prev_e: merged_itv_runs_lowBQ[-1] = (prev_s, max(prev_e, curr_e))
+			else: merged_itv_runs_lowBQ.append((curr_s, curr_e))
 
 		return merged_itv_runs_lowBQ
 
-def splitRead(title, seq, qual, min_len_seq, min_bq, max_len_lowbq, max_ratio_low_bq):
+def splitRead(title, seq, qual, min_len_seq, min_bq, max_len_lowbq_split, max_ratio_low_bq_split):
 
+	if len(seq) < min_len_seq: return []
+
+	lowBQIntervals = [] if max_ratio_low_bq_split >= 1.0 else getLowBQIntervals(qual, min_bq, max_len_lowbq_split, max_ratio_low_bq_split)
+
+	if len(lowBQIntervals) == 0: return [(title + "/0", replaceIUPAC(seq), qual)]
+
+	prev_pos = 0
+	id_seq = 0
+	
 	out = []
-	len_rec = len(seq)
 
-	if (len_rec >= min_len_seq):
+	for itv_s, itv_e in lowBQIntervals:
 
-		lowBQIntervals = getLowBQIntervals(qual, min_bq, max_len_lowbq, max_ratio_low_bq)
-		id_seq = 0
+		if itv_s - prev_pos >= min_len_seq:
 
-		if (len(lowBQIntervals) == 0): out.append((title + "/" + str(id_seq), replaceIUPAC(seq), qual))
-		else:
+			out.append((title + "/" + str(id_seq), replaceIUPAC(seq[prev_pos : itv_s]), qual[prev_pos : itv_s]))
 
-			prev_pos = 0
+			id_seq += 1
 
-			for itv in lowBQIntervals:
+		prev_pos = itv_e + 1
 
-				if (itv[0]-prev_pos >= min_len_seq):
-
-					out.append((title + "/" + str(id_seq), replaceIUPAC(seq[prev_pos:itv[0]]), qual[prev_pos:itv[0]]))
-
-					id_seq += 1
-
-				prev_pos = itv[1] + 1
-
-			if (len_rec-prev_pos >= min_len_seq): out.append((title + "/" + str(id_seq), replaceIUPAC(seq[prev_pos:]), qual[prev_pos:]))
+	if len(seq) - prev_pos >= min_len_seq: out.append((title + "/" + str(id_seq), replaceIUPAC(seq[prev_pos:]), qual[prev_pos:]))
 
 	return out
+	
+def splitReads(reads_l, min_len_seq, min_bq, max_len_lowbq_split, max_ratio_low_bq_split):
 
-def splitReads(reads, min_len_seq, min_bq, max_len_lowbq, max_ratio_low_bq):
+	return [splitRead(title, seq, qual, min_len_seq, min_bq, max_len_lowbq_split, max_ratio_low_bq_split) for title, seq, qual in reads_l]
+	
+def maskOrSplitRead(title, seq, qual, min_len_seq, min_bq, max_len_lowbq_split, max_len_lowbq_mask, max_ratio_low_bq_split, max_ratio_low_bq_mask):
 
-	return [splitRead(x[0], x[1], x[2], min_len_seq, min_bq, max_len_lowbq, max_ratio_low_bq) for x in reads]
+	if len(seq) < min_len_seq: return []
 
-def printRecord(title, seq, qual):
+	lowBQIntervals = [] if max_ratio_low_bq_mask >= 1.0 else getLowBQIntervals(qual, min_bq, max_len_lowbq_mask, max_ratio_low_bq_mask)
 
-	print("@" + title) # Name
-	print(seq) # Sequence
-	print("+")
-	print(qual) # Qualities
+	if len(lowBQIntervals) == 0: return splitRead(title, seq, qual, min_len_seq, min_bq, max_len_lowbq_split, max_ratio_low_bq_split)
 
-def printRecords(l_records):
+	seq_masked_l = list(seq)
 
-	for rec in l_records:
+	for itv_s, itv_e in lowBQIntervals:
+	
+		for i in range(itv_s, itv_e):
+		
+			if ord(qual[i]) - 33 < min_bq: seq_masked_l[i] = 'N'
+			
+	return splitRead(title, ''.join(seq_masked_l), qual, min_len_seq, min_bq, max_len_lowbq_split, max_ratio_low_bq_split)
+	
+def maskOrSplitReads(reads_l, min_len_seq, min_bq, max_len_lowbq_split, max_len_lowbq_mask, max_ratio_low_bq_split, max_ratio_low_bq_mask):
 
-		print("@" + rec[0]) # Name
-		print(rec[1]) # Sequence
-		print("+")
-		print(rec[2]) # Qualities
+	return [maskOrSplitRead(title, seq, qual, min_len_seq, min_bq, max_len_lowbq_split, max_len_lowbq_mask, max_ratio_low_bq_split, max_ratio_low_bq_mask) for title, seq, qual in reads_l]
 
 if __name__ == "__main__":
 
@@ -132,17 +141,22 @@ if __name__ == "__main__":
 	optional.add_argument('-c', '--chunk_per_thread', action='store', help='Number of FASTQ records per thread', type=int, default=1000, required=False)
 	optional.add_argument('-l', '--min_length', action='store', help='Minimum length of output reads', type=int, default=5000, required=False)
 	optional.add_argument('-b', '--min_bq', action='store', help='Minimum base quality', type=int, default=5, required=False)
-	optional.add_argument('-w', '--min_window_size', action='store', help='Minimum length of sliding windows', type=int, default=100, required=False)
-	optional.add_argument('-r', '--max_ratio_bq', action='store', help='Maximum ratio of low base quality within sliding windows for split', type=float, default=0.9, required=False)
+	optional.add_argument('-w', '--min_window_size_split', action='store', help='Minimum length of sliding windows for splitting', type=int, default=100, required=False)
+	optional.add_argument('-W', '--min_window_size_mask', action='store', help='Minimum length of sliding windows for masking', type=int, default=100, required=False)
+	optional.add_argument('-r', '--max_ratio_bq_split', action='store', help='Maximum ratio of low base quality within sliding windows to trigger split', type=float, default=0.9, required=False)
+	optional.add_argument('-R', '--max_ratio_bq_mask', action='store', help='Maximum ratio of low base quality within sliding windows to trigger mask', type=float, default=1.0, required=False)
 
 	args = parser.parse_args()
 
 	if (args.min_length < 1): raise SystemError("Minimum length of output reads cannot be less than 1.")
 	if (args.min_bq < 0): raise SystemError("Cannot use base quality less than 0.")
 	if (args.min_bq > 90): raise SystemError("Cannot use base quality greater than 90.")
-	if (args.min_window_size < 1): raise SystemError("Minimum length of sliding windows cannot be less than 1.")
-	if (args.max_ratio_bq < 0.0): raise SystemError("Maximum ratio of low base quality within sliding windows cannot be less than 0.0.")
-	if (args.max_ratio_bq > 1.0): raise SystemError("Maximum ratio of low base quality within sliding windows cannot be greater than 1.0.")
+	if (args.min_window_size_split < 1): raise SystemError("Minimum length of sliding windows for splitting cannot be less than 1.")
+	if (args.min_window_size_mask < 1): raise SystemError("Minimum length of sliding windows for masking cannot be less than 1.")
+	if (args.max_ratio_bq_split < 0.0): raise SystemError("Maximum ratio of low base quality within sliding windows to split cannot be less than 0.0.")
+	if (args.max_ratio_bq_split > 1.0): raise SystemError("Maximum ratio of low base quality within sliding windows to split cannot be greater than 1.0.")
+	if (args.max_ratio_bq_mask < 0.0): raise SystemError("Maximum ratio of low base quality within sliding windows to mask cannot be less than 0.0.")
+	if (args.max_ratio_bq_mask > 1.0): raise SystemError("Maximum ratio of low base quality within sliding windows to mask cannot be greater than 1.0.")
 
 	for fn in args.in_long_fastq:
 	
@@ -159,20 +173,20 @@ if __name__ == "__main__":
 
 					if (len(rec.sequence) >= args.min_length):
 
-						rec_out = splitRead(rec.name, rec.sequence, rec.quality, args.min_length, args.min_bq, args.min_window_size, args.max_ratio_bq)
+						rec_out = maskOrSplitRead(rec.name, rec.sequence, rec.quality, args.min_length, args.min_bq, args.min_window_size_split, args.min_window_size_mask, args.max_ratio_bq_split, args.max_ratio_bq_mask)
 
 						printRecords(rec_out)
 
 			else:
 
-				records_in = [ [] for _ in range(args.threads) ]
+				records_in = [[] for _ in range(args.threads)]
 				records_out = []
 
 				id_rec = 0
 
 				for rec in fh:
 
-					if (len(rec.sequence) >= args.min_length):
+					if len(rec.sequence) >= args.min_length:
 
 						records_in[id_rec % args.threads].append((rec.name, rec.sequence, rec.quality))
 
@@ -184,31 +198,31 @@ if __name__ == "__main__":
 
 							for i in range(args.threads):
 
-								thread_pool.apply_async(splitReads, args=(records_in[i], args.min_length, args.min_bq, args.min_window_size, args.max_ratio_bq), callback=records_out.append)
+								thread_pool.apply_async(maskOrSplitReads, args=(records_in[i], args.min_length, args.min_bq, args.min_window_size_split, args.min_window_size_mask, args.max_ratio_bq_split, args.max_ratio_bq_mask), callback=records_out.append)
 
 							thread_pool.close() 
 							thread_pool.join()
 
-							for i in range(args.threads):
+							for i in range(len(records_out)):
 
 								for rec_out in records_out[i]: printRecords(rec_out)
 
-							records_in = [ [] for _ in range(args.threads) ]
+							records_in = [[] for _ in range(args.threads)]
 							records_out = []
 
 							id_rec = 0
 
-				if (id_rec != 0):
+				if id_rec != 0:
 
 					thread_pool = mp.Pool(processes = args.threads)
 
 					for i in range(args.threads):
 
-						thread_pool.apply_async(splitReads, args=(records_in[i], args.min_length, args.min_bq, args.min_window_size, args.max_ratio_bq), callback=records_out.append)
+						thread_pool.apply_async(maskOrSplitReads, args=(records_in[i], args.min_length, args.min_bq, args.min_window_size_split, args.min_window_size_mask, args.max_ratio_bq_split, args.max_ratio_bq_mask), callback=records_out.append)
 
 					thread_pool.close() 
 					thread_pool.join()
 
-					for i in range(args.threads):
+					for i in range(len(records_out)):
 
 						for rec_out in records_out[i]: printRecords(rec_out)
